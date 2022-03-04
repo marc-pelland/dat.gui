8d6d6ea37ac7eb3b91a90ede1cc71365fbda3c85
Marc Pelland <marc@fireburnsup.com>
2022-03-04 09:53:21 -0500

    update document issues

--- src/dat/utils/css.js
+++ src/dat/utils/css.js
@@ -13,16 +13,18 @@
 
 const css = {
   load: function (url, indoc) {
-    const doc = indoc || document;
-    const link = doc.createElement('link');
-    link.type = 'text/css';
-    link.rel = 'stylesheet';
-    link.href = url;
-    doc.getElementsByTagName('head')[0].appendChild(link);
+    const doc = typeof document !== 'undefined' ? indoc || document : null;
+    if (doc !== null) {
+      const link = doc.createElement('link');
+      link.type = 'text/css';
+      link.rel = 'stylesheet';
+      link.href = url;
+      doc.getElementsByTagName('head')[0].appendChild(link);
+    }
   },
 
   inject: function (cssContent, indoc) {
-    const doc = indoc || document || null;
+    const doc = typeof document !== 'undefined' ? indoc || document : null;
     if (doc !== null) {
       const injected = document.createElement('style');
       injected.type = 'text/css';
